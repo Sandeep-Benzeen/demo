3382ee83940ac939e9a8e2e180882601a87fba4d
Sandeep-Benzeen <sandeep@benzeenautoparts.com>
2023-06-14 23:15:03 +0530

    tttt2227

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('./dist/HelloWorld'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/HelloWorld/'}),
+);
+
+app.listen(process.env.PORT || 8080);
